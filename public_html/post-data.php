84260b6405cd4e73a69cdb836e7990ef9ba8fa2f
DecodeTalker <78994554+Johansaputro@users.noreply.github.com>
2022-02-11 15:08:41 +0700

    Update post-data.php

--- public_html/post-data.php
+++ public_html/post-data.php
@@ -17,7 +17,7 @@ $dbname = "esp_data";
 // REPLACE with Database user
 $username = "root";
 // REPLACE with Database user password
-$password = "634630";
+$password = "";
 
 // Keep this API Key value to be compatible with the ESP32 code provided in the project page. If you change this value, the ESP32 sketch needs to match
 $api_key_value = "tPmAT5Ab3j7F9";
